c635732e834dde7dce9160d8136d2fc64d971ed4
Alex Vasilyev <olorin2007@gmail.com>
2022-08-06 21:52:01 +0200

    Initial Commit

---
+++ ScanBar/ScanBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanBarApp.swift
+//  ScanBar
+//
+//  Created by Alex Vasyliev on 06.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScanBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
