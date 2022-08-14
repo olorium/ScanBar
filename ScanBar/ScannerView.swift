131d2836ff31628480d01a6e05609cc001aeb91d
Alex Vasilyev <olorin2007@gmail.com>
2022-08-14 21:57:45 +0200

    Create a binding to show scanned code

--- ScanBar/ScannerView.swift
+++ ScanBar/ScannerView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ScannerView: UIViewControllerRepresentable {
+    
+    @Binding var scannedCode: String
    
     func makeUIViewController(context: Context) -> ScannerVC {
         ScannerVC(scannerDelegate: context.coordinator)
@@ -17,12 +19,19 @@ struct ScannerView: UIViewControllerRepresentable {
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(scannerView: self)
     }
     
     final class Coordinator: NSObject, ScannerVCDelegate {
+        
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
         func didFind(barcode: String) {
-            print(barcode)
+            scannerView.scannedCode = barcode
         }
         
         func didSurface(error: CameraError) {
@@ -33,6 +42,6 @@ struct ScannerView: UIViewControllerRepresentable {
 
 struct ScannerView_Previews: PreviewProvider {
     static var previews: some View {
-        ScannerView()
+        ScannerView(scannedCode: .constant("12344567890"))
     }
 }
